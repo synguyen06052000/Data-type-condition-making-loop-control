d28ae99802d47d512aeb3853dde726f3d8ffebf4
synguyen06052000 <86198106+synguyen06052000@users.noreply.github.com>
2022-04-04 15:03:27 +0700

    Add files via upload

--- Bai11.py
+++ Bai11.py
@@ -1,15 +1,14 @@
 import string
 class KiemTraThuanNghich:
     def __init__(self,st):
-        self.s = st
+        self.m_s = st
     def check(self):
-        String = self.s
-        for i in range(0,len(String)):
-            if(String[i]!=String[len(String)-i-1]):
+        S = self.m_s
+        for i in range(0,int(len(S)/2)):
+            if(S[i]!=S[len(S)-i-1]):
                 return("Khong la so thuan nghich")
-            if i >= (len(String)/2):
-                return("La so thuan nghich")
+        return("La so thuan nghich")
 if __name__ == "__main__":
-    N = (input("Nhap so nguyen N: "))
-    kiemTra = KiemTraThuanNghich(N)
-    print(N,kiemTra.check())
+    mS = (input("Nhap so nguyen N: "))
+    kiemTra = KiemTraThuanNghich(mS)
+    print(mS,kiemTra.check())

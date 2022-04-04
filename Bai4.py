28bf47de3cbd462be3aba0c76ea0aeb3030b310a
synguyen06052000 <86198106+synguyen06052000@users.noreply.github.com>
2022-04-04 15:37:54 +0700

    Add files via upload

--- Bai4.py
+++ Bai4.py
@@ -12,12 +12,6 @@ class SumN:
             if((i%2)!=0):
                 s1+=i
         return s1
-    def sum_chan_N(self):
-        s2 = 0
-        for i in range(1,self.so_N):
-            if((i%2)==0):
-                s2+=i
-        return s2
 if __name__=="__main__":
     N = (int)(input("Nhap so N:"))
     sumN = SumN(N)
@@ -25,5 +19,5 @@ if __name__=="__main__":
     
     print("Tong so tu nhien le khong qua N la S1=",sumN.sum_le_N())
  
-    print("Tong so tu nhien chan khong qua N la S2=",sumN.sum_chan_N())
+    print("Tong so tu nhien chan khong qua N la S2=",sumN.sum_N()-sumN.sum_le_N())
     
